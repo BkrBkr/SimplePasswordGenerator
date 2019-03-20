273b4453fe30fb050dbfdf3ab6c5de0677332c17
Björn Kremer <bjoern.kremer1@gmail.com>
2019-03-20 19:26:25 +0100

    Erste Version des Tools

---
+++ PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PasswordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.tbAmount = new System.Windows.Forms.TrackBar();
+            this.lblCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(12, 11);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.Size = new System.Drawing.Size(239, 20);
+            this.txtPW.TabIndex = 0;
+            // 
+            // tbAmount
+            // 
+            this.tbAmount.Location = new System.Drawing.Point(12, 38);
+            this.tbAmount.Maximum = 100;
+            this.tbAmount.Minimum = 2;
+            this.tbAmount.Name = "tbAmount";
+            this.tbAmount.Size = new System.Drawing.Size(239, 45);
+            this.tbAmount.TabIndex = 1;
+            this.tbAmount.Value = 10;
+            this.tbAmount.ValueChanged += new System.EventHandler(this.tbAmount_ValueChanged);
+            this.tbAmount.MouseUp += new System.Windows.Forms.MouseEventHandler(this.tbAmount_MouseUp);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(255, 45);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(19, 13);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 118);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.tbAmount);
+            this.Controls.Add(this.txtPW);
+            this.Name = "Form1";
+            this.Text = "SimplePasswordGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.tbAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.TrackBar tbAmount;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ PasswordGenerator/Form1.cs
@@ -0,0 +1,55 @@
+﻿//Copyright(C) 2019 Björn Kremer
+
+//This file Is part Of SimplePasswordGenerator.
+
+//SimplePasswordGenerator Is free software: you can redistribute it And/Or modify
+//it under the terms Of the GNU General Public License As published by
+//the Free Software Foundation, either version 3 Of the License, Or
+//(at your option) any later version.
+
+//SimplePasswordGenerator Is distributed In the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License For more details.
+
+//You should have received a copy Of the GNU General Public License
+//along with SimplePasswordGenerator. If Not, see < http: //www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            generate();
+            Clipboard.SetText(txtPW.Text);
+        }
+
+        private void tbAmount_ValueChanged(object sender, EventArgs e)
+        {
+            generate();
+        }
+        private void generate()
+        {
+            lblCount.Text = tbAmount.Value.ToString();
+            txtPW.Text = System.Web.Security.Membership.GeneratePassword(tbAmount.Value, 1);
+        }
+      
+        private void tbAmount_MouseUp(object sender, MouseEventArgs e)
+        {
+            Clipboard.SetText(txtPW.Text);
+            txtPW.Focus();
+        }
+    }
+}
